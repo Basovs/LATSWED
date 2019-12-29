88d7969e70e2eb80a8f63b02b30cce48ad71fe56
Janis Basovs <janisbasovs@Janiss-MacBook.lan>
2019-12-29 01:07:20 +0100

    sdfg

---
+++ main.js
@@ -0,0 +1,19 @@
+window.onscroll = function() {
+    changeOnScroll()
+}
+
+function changeOnScroll() {
+    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+        document.querySelector("nav").className = "onScroll"
+    } else {
+        document.querySelector("nav").className = ""
+    }
+}
+
+window.addEventListener("scroll", function() {
+    const target = document.querySelector(".parallax")
+    let scrolled = window.pageYOffset
+    let positiveRate = scrolled * 0.3
+
+    target.style.transform = `translate3d(0px, ${positiveRate}px, 0px)`
+})
