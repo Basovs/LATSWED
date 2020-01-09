e865cbf2c5c4dbef9658ec5b4966e6e51ca388fd
Janis Basovs <janisbasovs@Janiss-MacBook.local>
2020-01-09 10:38:19 +0100

    dfg

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ window.onscroll = function() {
 
 function changeOnScroll() {
     if (
-        document.body.scrollTop > 10 ||
-        document.documentElement.scrollTop > 0
+        document.body.scrollTop > 0 ||
+        document.documentElement.scrollTop > 10
     ) {
         document.querySelector("nav").className = "onScroll"
     } else {
