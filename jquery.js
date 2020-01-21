4a533a2f0a5694310e1de372affeecad759abe2a
Janis Basovs <janisbasovs@Janiss-MacBook.lan>
2020-01-21 19:48:08 +0100

    fixed hamburger il slide

--- jquery.js
+++ jquery.js
@@ -13,4 +13,18 @@ $(document).ready(function() {
     // $(".hamburger").click(function() {
     //     this.toggleClass("is-active")
     // })
+
+    //hamburger menu li slide
+    $(".hamburger-btn").click(function() {
+        $(".li_1").animate({ margin: "2rem 0 2rem 0" }, 150)
+        $(".li_2")
+            .delay(100)
+            .animate({ margin: "2rem 0 2rem 0" }, 150)
+        $(".li_3")
+            .delay(200)
+            .animate({ margin: "2rem 0 2rem 0" }, 150)
+        $(".li_4")
+            .delay(300)
+            .animate({ margin: "2rem 0 2rem 0" }, 150)
+    })
 })
